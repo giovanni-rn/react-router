fcd2f80895aa22d8605ebde11a09e95c5933467b
oriano-dev <nono5g@protonmail.com>
2022-11-12 18:34:27 +0100

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
-  const [number, setNumber] = useState(0)
-
     return (
       <div className="Home">
-        <div>
           Accueil
           <Navbar />
-        </div>
-        <div className="sandbox">
-          {number}
-        </div>
-        <button onClick={() => setNumber(number+1)}>
-          INCREMENTER
-        </button>
       </div>
     );
   }
